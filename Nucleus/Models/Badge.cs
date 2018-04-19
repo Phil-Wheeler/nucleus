3bf43806ca661f00d3f455276942e7cfee9211fc
phil.wheeler@intergen.co.nz <phil.wheeler@intergen.co.nz>
2018-04-19 17:29:16 +1200

    Home page UI update

---
+++ Nucleus/Models/Badge.cs
@@ -0,0 +1,21 @@
+﻿using Nucleus.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nucleus.Models
+{
+    public class Badge
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+    }
+
+    public class UserBadge
+    {
+        public virtual ApplicationUser User { get; set; }
+        public virtual Badge Badge { get; set; }
+    }
+}

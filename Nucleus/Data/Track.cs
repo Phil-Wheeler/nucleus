9556d89880ea2cd97d789d895e61ebe60c63f736
phil.wheeler@intergen.co.nz <phil.wheeler@intergen.co.nz>
2018-04-20 17:11:16 +1200

    Updates to models and layout

---
+++ Nucleus/Data/Track.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nucleus.Data
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

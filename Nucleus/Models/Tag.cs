4fe2838ad81c5cb5256d95c962d5f2ded078f3f9
phil.wheeler@intergen.co.nz <phil.wheeler@intergen.co.nz>
2018-04-24 13:43:29 +1200

    UI updates New Tags models

---
+++ Nucleus/Models/Tag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nucleus.Models
+{
+    /// <summary>
+    /// Tags are a taxonomical collection intended to describe personal circumstances such as current or aspirational roles, community allyships, etc.
+    /// </summary>
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// Tag sets help categorise tags into common groups or uses such as "Ethnicity", "Sexual Orientation", "Occupation", etc.
+    /// </summary>
+    public class TagSet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
